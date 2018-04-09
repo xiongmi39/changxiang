9223b8aa55f179eea82ce9105ee65aeb6a11213a
xiongmi <xiongmi39@163.com>
2018-04-09 18:28:52 +0800

    分享航班

--- pages/index/flightDetail/flightDetail.js
+++ pages/index/flightDetail/flightDetail.js
@@ -66,5 +66,21 @@ Page({
       startTime: startTime,
       reachTime: reachTime
     })
+  },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '自定义转发标题',
+      path: '/pages/main/main',
+      success: function(res) {
+        // 转发成功
+      },
+      fail: function(res) {
+        // 转发失败
+      }
+    }
   }
 })
--- pages/myCenter/myCenter.js
+++ pages/myCenter/myCenter.js
@@ -31,6 +31,22 @@ Page({
   },
   showCxmodal(){
     this.cxmodal.modalinput();
+  },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '畅想旅行',
+      path: '/pages/main/main',
+      success: function(res) {
+        // 转发成功
+      },
+      fail: function(res) {
+        // 转发失败
+      }
+    }
   }
 
 })
