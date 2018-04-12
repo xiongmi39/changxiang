b201e6155bbddac95d2bb226021b1e781d55252e
xiongmi <xiongmi39@163.com>
2018-04-12 15:21:03 +0800

    优化

--- pages/index/flightDetail/flightDetail.js
+++ pages/index/flightDetail/flightDetail.js
@@ -8,7 +8,8 @@ Page({
     endCity:"",
     realisland:"",
     startTime:"",
-    startDate:""
+    startDate:"",
+    status:"计划"
   },
   onReady: function () {
     //获得dialog组件
@@ -28,51 +29,11 @@ Page({
       flightInfo: JSON.parse(options.flightInfo)    
     })
     console.log(this.data.flightInfo);
-    this.handleflightInfo(this.data.flightInfo);
+    var flightInfo = app.util.handleflightInfo(this.data.flightInfo);
+    this.setData({    
+      flightInfo: flightInfo    
+    })    
 
-  },
-  handleflightInfo: function(info){
-    //城市
-    var citys = info.route_c.split("-");
-    var start = citys[0];
-    var end = citys[1];
-    //值机区，柜台
-    var islandarr = info.counters.split(",");
-    var realisland = [];
-    for(var i=0 ; i<islandarr.length;i++){
-      realisland.push(islandarr[i].slice(0,1));
-    }
-    realisland =realisland.join(",");
-    //出发时间，到达时间
-    var startTime = "";
-    var reachTime = "";
-    var startDate = "";
-    if(info.da_time){
-      startTime = info.da_time.split(" ")[1];
-      startDate = info.da_time.split(" ")[0];
-    }else if(info.de_time){
-      startTime = info.de_time.split(" ")[1];
-      startDate = info.de_time.split(" ")[0];
-    }else if(info.dp_time){
-      startTime = info.dp_time.split(" ")[1];
-      startDate = info.dp_time.split(" ")[0];
-    }
-
-    if(info.ap_time){
-      reachTime = info.ap_time.split(" ")[1];
-    }else if(info.ae_time){
-      reachTime = info.ae_time.split(" ")[1];
-    }else if(info.aa_time){
-      reachTime = info.aa_time.split(" ")[1];
-    }
-    this.setData({
-      startCity: start,
-      endCity: end,
-      realisland: realisland,
-      startTime: startTime,
-      reachTime: reachTime,
-      startDate:startDate
-    })
   },
   onShareAppMessage: function (res) {
     if (res.from === 'button') {
@@ -108,7 +69,7 @@ Page({
         sign: app.appConfig.getSign(app.appConfig.config.getAllFlightList,[{key:"flight_no",value:that.data.flightInfo.flight_no}])
       },
       success: function(res){
-        that.handleflightInfo(res.data.pd);
+        app.util.handleflightInfo(res.data.pd);
         that.setData({
           flightInfo: res.data.pd
         })
--- utils/util.js
+++ utils/util.js
@@ -23,41 +23,41 @@ var commonCheck = {
 	},
 	isMinAndMaxStr: function(data,min,max){
 		// if(eval(`/^.{${min},${max}}$/`).test(data)){
-		if(data.length > min && data.length <= max){
-		 
-			return true;
-		}else {
-			return false;
-		}
-	},	
-	isEqual: function(val1,val2){
-		if(val1 === val2){
-			return true;
-		}else {
-			return false;
-		}
-	},
-	isNoLetter: function(data){
-		if(/^[0-9a-zA-Z]*$/g.test(data)){
-			return true;
-		}else{
-			return false;
-		}
-	},
-	isID: function(id){
-		if(/^(\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$/.test(id)){
-			return true;
-		}else {
-			return false;
-		}
-	},
-	isRealName:function(data){
-	 	if(/[\u4e00-\u9fa5]+/.test(data)){
-	 		return true;
-	 	}else {
-	 		return false;
-	 	}
-	}
+      if(data.length > min && data.length <= max){
+
+       return true;
+     }else {
+       return false;
+     }
+   },	
+   isEqual: function(val1,val2){
+    if(val1 === val2){
+     return true;
+   }else {
+     return false;
+   }
+ },
+ isNoLetter: function(data){
+  if(/^[0-9a-zA-Z]*$/g.test(data)){
+   return true;
+ }else{
+   return false;
+ }
+},
+isID: function(id){
+  if(/^(\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$/.test(id)){
+   return true;
+ }else {
+   return false;
+ }
+},
+isRealName:function(data){
+ if(/[\u4e00-\u9fa5]+/.test(data)){
+  return true;
+}else {
+  return false;
+}
+}
 }
 
 function json2Form(json) { 
@@ -69,7 +69,56 @@ function json2Form(json) {
 } 
 
 function  handleFlightList(list){
-    list.forEach((item)=> {
+  list.forEach((item)=> {
+      // //出发，到达城市
+      // let citys = item.route_c.split("-");
+      // if(citys.length == 0){
+      //   return
+      // }
+      // let start = citys[0];
+      // let end = citys[1];
+      // item.startCity = start;
+      // item.endCity = end;
+
+      // //出发时间，到达时间
+      // var startTime = "";
+      // var reachTime = "";
+      // var startDate = "";
+      // var endDate = "";
+      // if(item.dp_time){
+      // 	startTime = item.dp_time.split(" ")[1];
+      // 	startDate = item.dp_time.split(" ")[0];
+      // }else if(item.de_time){
+      // 	startTime = item.de_time.split(" ")[1];
+      // 	startDate = item.de_time.split(" ")[0];
+      // }else if(item.da_time){
+      // 	startTime = item.da_time.split(" ")[1];
+      // 	startDate = item.da_time.split(" ")[0];
+      // }
+
+      // if(item.ap_time){
+      //   startTime = item.ap_time.split(" ")[1];
+      //   endDate = item.ap_time.split(" ")[0];
+      // }else if(item.ae_time){
+      //   startTime = item.ae_time.split(" ")[1];
+      //   endDate = item.ae_time.split(" ")[0];
+      // }else if(item.aa_time){
+      //   startTime = item.aa_time.split(" ")[1];
+      //   endDate = item.aa_time.split(" ")[0];
+      // }
+
+      // if(endDate.length == 0){
+      //   endDate = startDate;
+      // }
+      // item.startTime = startTime;
+      // item.reachTime = reachTime;
+      // item.startDate = startDate;
+      // item.endDate = endDate;
+      handleflightInfo(item);
+    });
+}
+
+function handleflightInfo(item){
       //出发，到达城市
       let citys = item.route_c.split("-");
       if(citys.length == 0){
@@ -80,20 +129,28 @@ function  handleFlightList(list){
       item.startCity = start;
       item.endCity = end;
 
+      //值机区，柜台
+      var islandarr = item.counters.split(",");
+      var realisland = [];
+      for(var i=0 ; i<islandarr.length;i++){
+        realisland.push(islandarr[i].slice(0,1));
+      }
+      item.realisland =realisland.join(",");
+
       //出发时间，到达时间
       var startTime = "";
       var reachTime = "";
       var startDate = "";
       var endDate = "";
-      if(item.da_time){
-        startTime = item.da_time.split(" ")[1];
-        startDate = item.da_time.split(" ")[0];
+      if(item.dp_time){
+      	startTime = item.dp_time.split(" ")[1];
+      	startDate = item.dp_time.split(" ")[0];
       }else if(item.de_time){
-        startTime = item.de_time.split(" ")[1];
-        startDate = item.de_time.split(" ")[0];
-      }else if(item.dp_time){
-        startTime = item.dp_time.split(" ")[1];
-        startDate = item.dp_time.split(" ")[0];
+      	startTime = item.de_time.split(" ")[1];
+      	startDate = item.de_time.split(" ")[0];
+      }else if(item.da_time){
+      	startTime = item.da_time.split(" ")[1];
+      	startDate = item.da_time.split(" ")[0];
       }
 
       if(item.ap_time){
@@ -114,12 +171,12 @@ function  handleFlightList(list){
       item.reachTime = reachTime;
       item.startDate = startDate;
       item.endDate = endDate;
+      return item
+    }
 
-    });
-  }
-
-module.exports = {
-	commonCheck:commonCheck,
-	json2Form:json2Form,
-	handleFlightList:handleFlightList
-}
+    module.exports = {
+     commonCheck:commonCheck,
+     json2Form:json2Form,
+     handleFlightList:handleFlightList,
+     handleflightInfo:handleflightInfo
+   }
